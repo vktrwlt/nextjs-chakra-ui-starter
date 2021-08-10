97f0d557b8a678876f38a10195051b6d131ba228
Victor Tsang <victorwltsang@gmail.com>
2021-08-10 15:21:40 -0700

    lint

--- src/components/_atoms/ChakraButton/ChakraButton.stories.js
+++ src/components/_atoms/ChakraButton/ChakraButton.stories.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { ChakraButton } from '~/components/_atoms/ChakraButton/ChakraButton'
+import React from 'react';
+import { ChakraButton } from '~/components/_atoms/ChakraButton/ChakraButton';
 
 export default {
   title: '_atoms/ChakraButton',
   component: ChakraButton,
-}
+};
 
-const Template = args => <ChakraButton {...args} />
+const Template = args => <ChakraButton {...args} />;
 
-export const Default = Template.bind({})
+export const Default = Template.bind({});
 Default.args = {
   size: 'md',
   colorScheme: 'teal',
   label: 'Button',
-}
+};
